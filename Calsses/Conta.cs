1586cd28f6ab28b57008b161f0aa3cc23ef81140
Josué Alves de Oliveira <josueao.mobile@gmail.com>
2021-07-01 18:44:50 -0300

    Criado os atributos e método construtor da class Conta e o enum TipoConta

--- Calsses/Conta.cs
+++ Calsses/Conta.cs
@@ -2,6 +2,18 @@ namespace DIO.Bank
 {
     public class Conta
     {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        public string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome) {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+
+        }
         
     }
 }
